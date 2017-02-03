c97ae2330c8851ac036bf510e7c1a41fbe8239b7
Juan <jvento@redisrupt.io>
2017-02-03 18:46:30 -0500

    add register-babel

---
+++ register-babel.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const babelrc = fs.readFileSync(path.resolve(__dirname, './.babelrc'));
+let config;
+
+try {
+  config = JSON.parse(babelrc);
+} catch (err) {
+  console.error('==> ERROR: Error parsing your .babelrc.');
+  console.error(err);
+}
+
+require('babel-register')(config);
